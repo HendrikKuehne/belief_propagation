Fixing numerical issues in L2BP compression

belief_propagation/PEPO.py
@@ -378,13 +378,23 @@ class PEPO:
         self.__chain_construction_recursion(edges_to_indices={},chains=operator_chains,sanity_check=sanity_check)
 
         # removing identity operators from the chain, substituting indices with operators
-        for chain in operator_chains:
-            keys = tuple(chain.keys())
-            for key in keys:
+        for iChain,chain in enumerate(operator_chains):
+            # any identities in the chain?
+            is_id = {node:np.allclose(self[node][chain[node]],self.I) for node in chain.keys()}
+
+            if all(is_id.values()):
+                # this chain consists of identities only, and needs special treatment
+                if remove_ids: operator_chains[iChain] = {self.root:chain[self.root]}
+
+                if save_tensors:
+                    for node in operator_chains[iChain].keys():
+                        operator_chains[iChain][node] = self[node][operator_chains[iChain][node]]
+
+            for key in is_id.keys():
                 T = self[key][chain[key]]
 
                 # removing identities
-                if remove_ids and np.allclose(T,self.I):
+                if remove_ids and is_id[key]:
                     chain.pop(key,None)
                     continue
 
@@ -426,8 +436,6 @@ class PEPO:
 
         # which nodes belonog to the unvisited edges?
         next_nodes = set().union(*[set(edge) for edge in next_edges])
-
-        assert sanity_check == sanity_check
 
         for neighbor_indices in itertools.product(*[range(self.G[edge[0]][edge[1]][0]["size"]) for edge in next_edges]):
             # preparing the next iteration
@@ -549,7 +557,6 @@ class PEPO:
         * Are the physical legs the last two dimensions in each tensor?
         * Do the physical legs have the correct sizes?
         * is the information flow in the tree intact?
-        * Is every constituent operator hermitian?
         """
         # are all the necessary attributes defined?
         assert hasattr(self,"D")
@@ -649,7 +656,6 @@ class PEPO:
         for node in self.G.nodes():
             axes = tuple(range(len(self.G.adj[node]))) + (len(self.G.adj[node])+1,len(self.G.adj[node]))
             if not np.allclose(self[node],np.transpose(self[node],axes=axes)):
-                self.view_site(node)
                 return False
 
         return True
@@ -742,11 +748,19 @@ class PEPO:
         if not np.isscalar(x): raise ValueError("x must be a scalar.")
         newPEPO = copy.deepcopy(self)
 
-        N = newPEPO.nsites
-        for node in newPEPO: newPEPO[node] = newPEPO[node] * (x**(1/N))
-
         # since we are inserting additional factors into the PEPO, the tree traversal check will fail
         newPEPO.check_tree = False
+
+        # what we ae really doing is multiplying every operator chain by x;
+        # this is more computationaly intensive, but has the advantage that
+        # the sanity check still works (otherwise, identity operators
+        # would be multiplied by x, which makes the sanity check fail)
+        chains = newPEPO.operator_chains(save_tensors=False)
+
+        for chain in chains:
+            for node,index in chain.items():
+                newPEPO[node][index] *= x
+                break
 
         return newPEPO
 

belief_propagation/PEPS.py
@@ -7,7 +7,7 @@ import networkx as nx
 import cotengra as ctg
 import warnings
 import copy
-from typing import Iterator
+from typing import Union,Iterator
 
 from belief_propagation.utils import network_message_check,crandn,write_exp_bonddim_to_graph,multi_tensor_rank
 
@@ -103,6 +103,9 @@ class PEPS:
             if not "size" in data.keys():
                 warnings.warn(f"No size saved in edge ({node1},{node2}).",RuntimeWarning)
                 return False
+            if data["size"] == 0:
+                warnings.warn(f"Edge ({node1},{node2}) has size 0.",RuntimeWarning)
+                return False
             if data["size"] != self[node1].shape[data["legs"][node1]] or data["size"] != self[node2].shape[data["legs"][node2]]:
                 warnings.warn(f"Wrong size saved in edge ({node1},{node2}).",RuntimeWarning)
                 return False
@@ -135,7 +138,7 @@ class PEPS:
         return True
 
     @classmethod
-    def init_random(cls,G:nx.MultiGraph,D:int,chi:int,rng:np.random.Generator=np.random.default_rng(),real:bool=False,bond_dim_strategy:str="uniform",sanity_check:bool=False,**kwargs):
+    def init_random(cls,G:nx.MultiGraph,D:int,chi:int,rng:np.random.Generator=np.random.default_rng(),real:bool=False,bond_dim_strategy:str="uniform",sanity_check:bool=False,**kwargs) -> "PEPS":
         """
         Initializes a MPS randomly. The virtual bond dimension is `chi`,
         the physical dimension is `D`. Any leg ordering in `G` is not
@@ -169,7 +172,7 @@ class PEPS:
         return cls(G,sanity_check=sanity_check)
 
     @classmethod
-    def init_from_TN(cls,G:nx.MultiGraph,sanity_check:bool=False):
+    def init_from_TN(cls,G:nx.MultiGraph,sanity_check:bool=False) -> "PEPS":
         """
         Initialises a PEPS from a TN by appending dummy physical dimensions
         of size one to the site tensors.
@@ -186,7 +189,7 @@ class PEPS:
         return cls(G=newG,sanity_check=sanity_check)
 
     @classmethod
-    def Dummy(cls,G:nx.MultiGraph,sanity_check:bool=False):
+    def Dummy(cls,G:nx.MultiGraph,sanity_check:bool=False) -> "PEPS":
         """
         Returns a dummy PEPS on graph `G` with physical dimension one.
         """
@@ -199,6 +202,44 @@ class PEPS:
         for node1,node2 in G.edges(): G[node1][node2][0]["size"] = 1
 
         return cls(G=G,sanity_check=sanity_check)
+
+    @classmethod
+    def ProductState(cls,G:nx.MultiGraph,state:Union[np.ndarray,dict[int,np.ndarray]],normalize:bool=True,sanity_check:bool=False) -> "PEPS":
+        """
+        Initialises a product state PEPO. If `state` is an array, it is
+        broadcasted to all sites. If `state` is a dictionary, it is
+        assumed to contain a pure state for every site. The PEPO is
+        normalized to unity, if `normalize=True` (default).
+        """
+        if isinstance(state,np.ndarray):
+            return cls.ProductState(
+                G=G,
+                state={node:state for node in G.nodes()},
+                sanity_check=sanity_check
+            )
+
+        if isinstance(state,dict):
+            # sanity check
+            if not nx.utils.nodes_equal(state.keys(),G.nodes()): raise ValueError("State and graph do not agree in the nodes that they contain.")
+            if any([state[node].ndim != 1 for node in G.nodes()]): raise ValueError("All local states must be pure states.")
+
+            psi = cls.Dummy(G=G,sanity_check=sanity_check)
+
+            for node in psi:
+                old_shape = list(psi[node].shape)
+                old_shape[-1] = state[node].shape[0]
+                newT = np.resize(psi[node],old_shape)
+                newT[...,:] = state[node] / np.sqrt(np.dot(state[node].conj(),state[node])) if normalize else state[node]
+                psi[node] = newT
+
+            # new physical dimension
+            psi.D = state[node].shape[0]
+
+            if sanity_check: assert psi.intact
+
+            return psi
+
+        raise NotImplementedError("PEPS.ProductState not implemented for state of type " + str(type(state)) + ".")
 
     @staticmethod
     def prepare_graph(G:nx.MultiGraph,keep_legs:bool=False,keep_size:bool=False,sanity_check:bool=False) -> nx.MultiGraph:
@@ -309,14 +350,14 @@ class PEPS:
         newPEPS = copy.deepcopy(self)
 
         N = newPEPS.nsites
-        for node in newPEPS.G.nodes(): newPEPS.G.nodes[node]["T"] = newPEPS.G.nodes[node]["T"] * (x**(1/N))
+        for node in newPEPS: newPEPS[node] = newPEPS[node] * (x**(1/N))
 
         return newPEPS
 
     def __rmul__(self,x:float): return self.__mul__(x)
 
     def __repr__(self) -> str:
-        out = f"State on {self.nsites} sites. PEPS is " + ("intact." if self.intact else "not intact.")
+        out = f"State on {self.nsites} sites. Local Hilbert Space of size {self.D}. PEPS is " + ("intact." if self.intact else "not intact.")
         return out
 
         # bond dimensions and multilinear tensor ranks

belief_propagation/braket.py
@@ -609,7 +609,13 @@ class Braket(BaseBraket):
                         # self.__contract_tensors_inbound_messages
                         continue
 
-                    self.msg[sending_node][receiving_node] /= np.sum(self.msg[sending_node][receiving_node])
+                    msg_sum = np.sum(self.msg[sending_node][receiving_node])
+
+                    if np.isclose(msg_sum,0):
+                        warnings.warn(f"Message from {sending_node} to {receiving_node} sums to zero. Skipping normalization.",RuntimeWarning)
+                        continue
+
+                    self.msg[sending_node][receiving_node] /= msg_sum
 
             return
 

belief_propagation/truncate.py
@@ -140,7 +140,7 @@ def QR_bottleneck(braket:Braket,node1:int,node2:int,vec1:np.ndarray,vec2:np.ndar
 
     return
 
-def L2BP_compression(psi:PEPS,singval_threshold:float=1e-10,overlap:Braket=None,return_singvals:bool=True,sanity_check:bool=False,**kwargs) -> tuple[PEPS,dict[frozenset,np.ndarray]]:
+def L2BP_compression(psi:PEPS,singval_threshold:float=1e-10,overlap:Braket=None,return_singvals:bool=True,min_bond_dim:int=1,sanity_check:bool=False,**kwargs) -> tuple[PEPS,dict[frozenset,np.ndarray]]:
     """
     L2BP compression from [Sci. Adv. 10, eadk4321 (2024)](https://doi.org/10.1126/sciadv.adk4321).
     Singular values below `singval_threshold` are discarded. A `Braket` object can be passed, in which
@@ -152,7 +152,6 @@ def L2BP_compression(psi:PEPS,singval_threshold:float=1e-10,overlap:Braket=None,
 
     # handling kwargs
     kwargs["iterator_desc_prefix"] = (kwargs["iterator_desc_prefix"] + " | L2BP compression") if "iterator_desc_prefix" in kwargs.keys() else "L2BP compression"
-    kwargs["sanity_check"] = sanity_check
 
     if overlap != None:
         if not isinstance(overlap,Braket): raise ValueError("overlap is not a braket object!")
@@ -164,7 +163,7 @@ def L2BP_compression(psi:PEPS,singval_threshold:float=1e-10,overlap:Braket=None,
     if overlap == None:
         # BP iteration
         overlap = Braket.Overlap(psi,psi,sanity_check=sanity_check)
-        overlap.BP(**kwargs)
+        overlap.BP(**kwargs,sanity_check=sanity_check)
 
     all_singvals = {}
 
@@ -189,18 +188,27 @@ def L2BP_compression(psi:PEPS,singval_threshold:float=1e-10,overlap:Braket=None,
 
         # SVD over the bond, and truncation
         U,singvals,Vh = scialg.svd(R1 @ R2,full_matrices=False,overwrite_a=True)
-        nonzero_mask = np.logical_not(np.isclose(singvals,0,atol=singval_threshold))
 
         # saving singular values
         all_singvals[frozenset((node1,node2))] = singvals
 
-        if np.sum(nonzero_mask) == 0:
-            warnings.warn(f"Threshold {singval_threshold:.3e} cuts edge ({node1},{node2}). Setting bond dimension to one.")
-            nonzero_mask[0] = True
+        # for numerical stability, we have to drop all singular values that are close to zero
+        nonzero_mask = singvals != 0
+        # singular values we want to keep, based on singval_threshold
+        threshold_mask = np.logical_not(np.isclose(singvals,0,atol=singval_threshold))
 
-        U = U[:,nonzero_mask]
-        Vh = Vh[nonzero_mask,:]
-        singvals = singvals[nonzero_mask]
+        keep_mask = np.logical_and(nonzero_mask,threshold_mask)
+
+        if sum(nonzero_mask) == 0: raise RuntimeError(f"Edge ({node1},{node2}) is zero-valued.")
+
+        if np.sum(keep_mask) < min_bond_dim and sum(nonzero_mask) > 0:
+            # the singular value threshold is too restrictive
+            for i in range(min(min_bond_dim,sum(nonzero_mask))):
+                keep_mask[i] = True
+
+        U = U[:,keep_mask]
+        Vh = Vh[keep_mask,:]
+        singvals = singvals[keep_mask]
 
         # projectors
         P1 = np.einsum("ij,jk,kl->il",R2,Vh.conj().T,np.diag(1 / np.sqrt(singvals)),optimize=True)